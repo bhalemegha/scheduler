c5e8f212903ba0bf8f7c9b2c3e9f09dd995e128d
bhalemegha <megha.bhale6@gmail.com>
2021-12-06 21:30:07 -0600

    initial

--- stories/index.js
+++ stories/index.js
@@ -13,7 +13,7 @@ storiesOf("Button", module)
   })
   .add("Base", () => <Button>Base</Button>)
   .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Danger", () => <Button danger >Cancel</Button>)
   .add("Clickable", () => (
     <Button onClick={action("button-clicked")}>Clickable</Button>
   ))
