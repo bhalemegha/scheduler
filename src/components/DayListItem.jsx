f7a5b79f7b9d5656571940bad849ac343308a72d
bhalemegha <megha.bhale6@gmail.com>
2021-12-07 18:26:36 -0600

    Test DayListItem component

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -2,9 +2,23 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
+const formatSpots = (spots) => {
+  let displayText = "";
+  console.log(spots);
+  switch(Number(spots)) {
+    case 0 :
+      displayText = `no spots remaining`;
+      break;
+    case 1:
+      displayText = spots + ` spot remaining`;
+      break;
+    default:
+      displayText = spots + ` spots remaining`;
+  }
+  return displayText;
+}
 export default function DayListItem(props) {
   const {name, spots} = props;
-  console.log("Happy---");
   const setStyle = classNames("day-list__item",{
     "day-list__item--selected" : props.selected,
     "day-list__item--full" : props.full
@@ -12,7 +26,7 @@ export default function DayListItem(props) {
   return (
     <li className={setStyle}>
       <h2 className="text--regular">{name}</h2>
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
