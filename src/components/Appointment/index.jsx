212e84178ee49225cafb0b292edc2e1297fe2398
bhalemegha <megha.bhale6@gmail.com>
2021-12-08 23:54:21 -0600

    Display Appointments

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Show from "./Show";
+import Empty from "./Empty";
+import Header from "./Header";
 export default function Appointment(props) {
   const { time } = props;
+
   let availableAppointments = "";
-  function test() {
-    if (time) {
-      availableAppointments = "Appointment at " + time ;
-    } else {
-      availableAppointments = "No Appointments";
-    }
-    return availableAppointments;
-  }
+  // function test() {
+  //   if (time) {
+  //     availableAppointments = "Appointment at " + time ;
+  //   } else {
+  //     availableAppointments = "No Appointments";
+  //   }
+  //   return availableAppointments;
+  // }
   return (
-    <article className="appointment">{test()}</article>
+    <article className="appointment">
+    <Header time={props.time}/>
+    {props.interview? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty></Empty> } 
+    </article>
   );
 }
