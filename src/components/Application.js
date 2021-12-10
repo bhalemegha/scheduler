acf612c6da336a143342061a59067b3fc45def9b
bhalemegha <megha.bhale6@gmail.com>
2021-12-09 21:19:54 -0600

    fetch days from scheduler-api

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
+import axios from "axios";
 const appointments = [
   {
     id: 1,
@@ -41,29 +42,19 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application() {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   const appointmentList = appointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />
   });
+
+  useEffect(()=>{
+    axios.get("/api/days")
+    .then((response)=> {
+      setDays(response.data);
+    })
+  },[]);
 
   return (
     <main className="layout">
