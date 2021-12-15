8f9c96ca544983cbed284fa8c033fb1b282f9a39
bhalemegha <megha.bhale6@gmail.com>
2021-12-15 15:12:24 -0600

    Initial test for DayItem using Cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,6 +1,20 @@
+/* eslint-disable no-undef */
 describe("Navigation", () => {
   it("should visit root", () => {
     // eslint-disable-next-line no-undef
     cy.visit("/");
+  });  
+  
+  it("should navigate to Tuesday", () => {
+    // eslint-disable-next-line no-undef
+    cy.visit("/");
+    cy.contains("Tuesday").should("to.be","Tuesday")
+  });
+
+  it("should Click to Tuesday", () => {
+    // eslint-disable-next-line no-undef
+    cy.visit("/");
+    cy.contains("Tuesday").as("dayItem");
+    cy.get("@dayItem").click()
   });
 });
