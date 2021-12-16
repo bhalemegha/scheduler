8e9825256884630c5b0483164c28278de5d211b6
bhalemegha <megha.bhale6@gmail.com>
2021-12-16 14:50:32 -0600

    Fix issues with test case raised after adding validation for interviewer list on form

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import shallow from 'enzyme';
 import { render, cleanup } from "@testing-library/react";
 import { fireEvent } from "@testing-library/react";
 // import {getByTestId} from '@testing-library/dom'
@@ -49,45 +49,31 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
 
-  it("calls onSave function when the name is defined", () => {
+  it("validates that the interviewer must be selected", () => {
     const onSave = jest.fn();
-
-    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-    const { getByText, queryByText } = render(
+    const { getByText } = render(
       <Form
         interviewers={interviewers}
         onSave={onSave}
         student="Lydia Miller-Jones"
       />
-    );
+    );  
+    const btnSave = getByText("Save");
+    fireEvent.click(btnSave);
+    /* 1. validation is shown */
+    // eslint-disable-next-line no-undef
+    expect(getByText(/Please select Interviewer/i)).toBeInTheDocument();
 
-    /* 3. Click the save button */
-    fireEvent.click(getByText("Save"));
-
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    /* 2. onSave is not called */
+    expect(onSave).not.toHaveBeenCalled();
   });
-  it("submits the name entered by the user", () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
-    );
 
-    const input = getByPlaceholderText("Enter Student Name");
-
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
-    fireEvent.click(getByText("Save"));
-
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-  });
-  //Not deleting above 2 tests even below test is covering those aspects...
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
-    const { getByText, getByPlaceholderText, queryByText } = render(
+    const { getByText, getByPlaceholderText, queryByText, getByAltText } = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
+    fireEvent.click(getByAltText("Sylvia Palmer"));
 
     fireEvent.click(getByText("Save"));
 
@@ -103,7 +89,7 @@ describe("Form", () => {
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
 
     expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
   
   it("calls onCancel and resets the input field", () => {
