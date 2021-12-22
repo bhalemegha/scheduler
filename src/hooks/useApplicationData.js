808c26a582bfd73472e355e4b5908de5ff49e89b
bhalemegha <megha.bhale6@gmail.com>
2021-12-21 23:40:40 -0600

    updated URls

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -14,9 +14,9 @@ export default function useApplicationData() {
   //It will call Api once as we are passing an empty array as dependency which is never going to change
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
@@ -48,7 +48,7 @@ export default function useApplicationData() {
       ...state.appointments,
       [id]: appointment
     };
-    axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
+    axios.put(`/api/appointments/${id}`, appointment)
     .then(() => {
       const days = updateSpots(id, mode);
       setState(prev => ({
@@ -72,7 +72,7 @@ export default function useApplicationData() {
     };
     // const days = updateSpots(id, false);
 
-    axios.delete(`http://localhost:8001/api/appointments/${id}`)
+    axios.delete(`/api/appointments/${id}`)
       .then(() => {
         const days = updateSpots(id, false);
         setState(prev => ({
